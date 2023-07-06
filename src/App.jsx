cab860151169a969296b75911bda127fc7e2dcc4
Elton <eltonfirminomarcelino@gmail.com>
2023-07-06 14:27:20 -0300

    criando lista de fruta

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+function App() {
+  const [fruitList, setFruit] = useState[
+    
+      {
+        name: "Morango",
+        price: 15,
+        category: "Frutas",
+     },
+     {
+        name: "Uva",
+        price: 23,
+        category: "Frutas",
+     },
+     {
+        name: "Banana",
+        price: 23,
+        category: "Frutas",
+     },
+     {
+        name: "Beterraba",
+        price: 5,
+        category: "Legumes",
+     }, 
+    
+  ]
+
+  
+  return (
+  
+  )
+}
+
+export default App
