441fca189b3e82f8a8e0b82641e19938f5358d1e
Elton <eltonfirminomarcelino@gmail.com>
2023-07-07 21:26:44 -0300

    Listando legumes e fazendo botão adicionar

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,76 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
   const [fruitList, setFruit] = useState([
-    
-      {
-        name: "Morango",
-        price: 15,
-        category: "Frutas",
-     },
-     {
-        name: "Uva",
-        price: 23,
-        category: "Frutas",
-     },
-     {
-        name: "Banana",
-        price: 23,
-        category: "Frutas",
-     },
-     {
-        name: "Beterraba",
-        price: 5,
-        category: "Legumes",
-     }, 
-    
-  ]) 
+    {
+      name: "Morango",
+      price: 15,
+      category: "Frutas",
+    },
+    {
+      name: "Uva",
+      price: 23,
+      category: "Frutas",
+    },
+    {
+      name: "Banana",
+      price: 23,
+      category: "Frutas",
+    },
+    {
+      name: "Beterraba",
+      price: 5,
+      category: "Legumes",
+    },
+  ]);
 
-  const total = fruitList.reduce((currentValue, nextValue) =>  currentValue + nextValue.price,0)
-console.log(total)
+  const total = fruitList.reduce(
+    (currentValue, nextValue) => currentValue + nextValue.price,
+    0
+  );
 
-const fruits = fruitList.filter(fruitValue=>(fruitValue.category ==="Frutas"))
-console.log(fruits)
+  const fruit1 = fruitList.filter(
+    (fruitValue) => fruitValue.category === "Frutas"
+  );
 
+  const fruit2 = fruitList.filter(
+    (fruitValue) => fruitValue.category === "Legumes"
+  );
 
-
-
-
+  const add = () => {
+    const newList = { name: "Beterraba", price: 5, category: "Legumes" };
+    setFruit([...fruitList, newList]);
+  };
 
   return (
     <div>
       <>
-       
-        {fruitList.map((fruit, index)=>(
+        {fruitList.map((fruit, index) => (
           <ul key={index}>
-        <li>{fruit.name}</li>
-        </ul>
-      ))}
+            <li>{fruit.name}</li>
+          </ul>
+        ))}
 
+        <button onClick={add}>adicionar</button>
 
-      <span>{`total:(${total})`}</span>
+        <h2>frutas</h2>
+        {fruit1.map((fruit, index) => (
+          <ul key={index}>
+            <li>{fruit.name}</li>
+          </ul>
+        ))}
 
-          {fruits.map((fruit, index) =>(
-             <ul key={index}>
-              <span>{fruit.name}</span>
-             </ul>
-             
-          ))}
-     
+        <h2>legumes</h2>
+        {fruit2.map((fruit, index) => (
+          <ul key={index}>
+            <li>{fruit.name}</li>
+          </ul>
+        ))}
+
+        <span>{`total:(${total})`}</span>
       </>
-     
-        
-      
     </div>
-      
-
-    
-  )
+  );
 }
 
-export default App
+export default App;
